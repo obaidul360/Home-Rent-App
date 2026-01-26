8328e1d29ca555c3f54e9b38f706f1f8916305c5
Md. Obaidul Islam <obdcse360@gmail.com>
2026-01-26 22:38:09 +0600

    upate text

--- lib/test/for_test.dart
+++ lib/test/for_test.dart
@@ -10,28 +10,18 @@ class ForTest extends StatefulWidget {
 class _ForTestState extends State<ForTest> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("For Test")),
-    body: Column(
+    return Scaffold(
+      appBar: AppBar(title: Text("For Test")),
+      body: Column(
         children: [
           Text("jnhvgvgvgv"),
           Text("for testv"),
           Text("jnhvgvgvgv"),
-          Row(
-            children: [
-              Text("for test"),
-              Text("for test"),
-              Text("for test"),
-            ],
-          ),
-          Row(
-            children: [
-              Text(" test"),
-              Text("test"),
-              Text(" test"),
-            ],
-          ),
-
-      ],
-    ),);
+          Row(children: [Text("for test"), Text("for test"), Text("for test")]),
+          Row(children: [Text("for test"), Text("for test"), Text("for test")]),
+          Row(children: [Text(" test"), Text("test"), Text(" test")]),
+        ],
+      ),
+    );
   }
 }
